0e99dd477ccb61c3d8c7926be8514945a7d262ef
dcoa <dcoa@live.com>
2020-11-22 15:36:01 -0500

    initial configuration

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD3MepqILYOvlCnTxdFZ_XVh9CUq5-EmNs",
+    authDomain: "treinta-1ab4f.firebaseapp.com",
+    databaseURL: "https://treinta-1ab4f.firebaseio.com",
+    projectId: "treinta-1ab4f",
+    storageBucket: "treinta-1ab4f.appspot.com",
+    messagingSenderId: "839599330069",
+    appId: "1:839599330069:web:160a1d482211740f2b0f1c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
